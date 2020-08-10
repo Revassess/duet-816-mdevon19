6ad59835e0dafe8aa12d63d61102272d9dcec8d2
Devon <dwmst291@mail.rmu.edu>
2020-08-10 16:53:17 -0400

    Just checking my point total

--- Revassess/src/main/java/com/revature/config/ConnectionUtil.java
+++ Revassess/src/main/java/com/revature/config/ConnectionUtil.java
@@ -11,6 +11,7 @@ import java.sql.Connection;
  *         CHANGE THE MODIFIERS OR THE NAMES OF THE VARIABLES. These are used to
  *         test your db schema.
  */
+//
 public class ConnectionUtil {
 	//for singleton instance
 	private static ConnectionUtil cu;
